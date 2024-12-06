0827e732b5623d55cee739b88ab01a2c73f052c3
Your <184955262+yourluckystar@users.noreply.github.com>
2024-12-07 04:38:58 +0900

    socket yis

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const socket = require('socket.io');
 const https = require('https');
+const path = require('path');
 const fs = require('fs');
 
 const app = express();
@@ -10,6 +11,24 @@ const options = {
 };
 
 const server = https.createServer(options, app);
+const io = socket(server);
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+io.on('connection', (socket) => {
+  console.log('A user connected');
+  io.emit('chatMessage', 'someone joined the room');
+
+  socket.on('chatMessage', (message) => {
+    io.emit('chatMessage', message);
+  });
+
+  socket.on('disconnect', () => {
+    console.log('A user disconnected');
+    io.emit('chatMessage', 'someone left the room');
+  });
+});
+
 
 server.listen(11945, () => {
     console.log('Server started at https://localhost:11945');
