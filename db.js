a9a2933a876a5f12d1f54d034ea6f5f7fa73b91b
Your <184955262+yourluckystar@users.noreply.github.com>
2025-01-11 03:04:47 +0900

    add db connect

---
+++ db.js
@@ -0,0 +1,21 @@
+import mysql from 'mysql2';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const db = mysql.createConnection({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+});
+
+db.connect((err) => {
+  if (err) {
+    console.error('couldnt connect to db :(', err.stack);
+    return;
+  }
+  console.log('connected to db .3');
+});
+
+export { db };
