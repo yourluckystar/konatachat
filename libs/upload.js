534abf129d0e0b9184c527040fe789f9950b6d0f
Your <184955262+yourluckystar@users.noreply.github.com>
2025-02-25 07:04:36 +0900

    .

---
+++ libs/upload.js
@@ -0,0 +1,36 @@
+import crypto from 'crypto';
+import path from 'path';
+import fs from 'fs';
+
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+export const save_files = async (files) => {
+    const files_arr = Object.values(files);
+    const file_path = path.join(__dirname, 'media');
+    const saved_paths = [];
+
+    for (const file of files_arr) {
+        let file_name;
+        let full_path;
+
+        do {
+            const random = crypto.randomBytes(16).toString('hex');
+            file_name = `${random}.${file.extension}`;
+            full_path = path.join(file_path, file_name);
+        } while (fs.existsSync(full_path));
+
+        try {
+            await fs.promises.writeFile(full_path, file.buffer);
+
+            const relative_path = path.relative(__dirname, full_path);
+            saved_paths.push(relative_path);
+        } catch (err) {
+            console.log('error writing file:', err);
+        }
+    }
+
+    return saved_paths;
+}
