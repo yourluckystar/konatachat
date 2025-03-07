c8bd5a196c76bf95745d4df0fc47edbc999e1208
Your <184955262+yourluckystar@users.noreply.github.com>
2025-03-08 01:55:00 +0900

    add database uploading

--- libs/upload.js
+++ libs/upload.js
@@ -3,13 +3,14 @@ import path from 'path';
 import fs from 'fs';
 
 import { fileURLToPath } from 'url';
+import { pool } from '../db.js';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 export const save_files = async (files) => {
     const files_arr = Object.values(files);
-    const file_path = path.join(__dirname, 'files');
+    const file_path = path.join('files');
     const saved_paths = [];
 
     for (const file of files_arr) {
@@ -23,8 +24,7 @@ export const save_files = async (files) => {
         } while (fs.existsSync(full_path));
 
         try {
-            await fs.promises.writeFile(full_path, file.buffer);
-
+            await fs.promises.writeFile(full_path, file.data);
             const relative_path = path.relative(__dirname, full_path);
             saved_paths.push(relative_path);
         } catch (err) {
@@ -34,3 +34,17 @@ export const save_files = async (files) => {
 
     return saved_paths;
 }
+
+export const update_database = async (data) => {
+    const attachments = data.files && data.files.length > 0;
+    console.log(attachments);
+    
+    const [result] = await pool.promise().query('INSERT INTO messages (sender, content, attachments) VALUES (?, ?, ?)', [data.user, data.content, attachments]);
+    const message = result.insertId;
+
+    if (attachments) {
+        for (const file_path of data.files) {
+            await pool.promise().query('INSERT INTO message_files (message, url) VALUES (?, ?)', [message, file_path]);
+        }
+    }
+};
